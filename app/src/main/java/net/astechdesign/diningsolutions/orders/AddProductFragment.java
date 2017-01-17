8a7f832273ab40da631a798f703786cdeae785dd
aaronrs <asouthwell@equalexperts.com>
2017-01-17 14:56:00 +0000

    pre-simplifying

--- app/src/main/java/net/astechdesign/diningsolutions/orders/AddProductFragment.java
+++ app/src/main/java/net/astechdesign/diningsolutions/orders/AddProductFragment.java
@@ -14,11 +14,12 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.FilterQueryProvider;
 
 import net.astechdesign.diningsolutions.R;
-import net.astechdesign.diningsolutions.database.tables.ProductCursorWrapper;
 import net.astechdesign.diningsolutions.database.tables.ProductTable;
 import net.astechdesign.diningsolutions.model.Product;
 import net.astechdesign.diningsolutions.repositories.ProductRepo;
@@ -36,10 +37,20 @@ public class AddProductFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_add_product, null);
-        AutoCompleteTextView productDropdown = (AutoCompleteTextView) view.findViewById(R.id.product_name_select);
+        final AutoCompleteTextView productDropdown = (AutoCompleteTextView) view.findViewById(R.id.product_name_select);
         productDropdown.setThreshold(3);
         productDropdown.setAdapter(getProductsAdapter());
+        productDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setTitle(R.string.add_product_title)
@@ -47,7 +58,8 @@ public class AddProductFragment extends DialogFragment {
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Product product = new Product(1, "name", "desc", 10.0, "1234", 0);
+                        String name = productDropdown.getText().toString();
+                        Product product = new Product(1, name, "desc", 10.0, "1234", 0);
                         mListener.onDialogPositiveClick(dialog, product);
                     }
                 })
@@ -57,29 +69,37 @@ public class AddProductFragment extends DialogFragment {
     private SimpleCursorAdapter getProductsAdapter() {
         final Context context = getContext().getApplicationContext();
         final ProductRepo productRepo = ProductRepo.get(context);
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(
+                context,
+                R.layout.product_dropdown,
+                productRepo.getProductCursor(),
+                new String[]{ProductTable.PRODUCT_NAME, ProductTable.PRODUCT_PRICE},
+                new int[]{R.id.product_name, R.id.product_price},
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        );
+        simpleCursorAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                return false;
+            }
+        });
         FilterQueryProvider filter = new FilterQueryProvider() {
             @Override
             public Cursor runQuery(CharSequence constraint) {
                 if (constraint != null) {
                     return productRepo.getProductCursor(constraint);
                 }
-                return null;
+                return productRepo.getProductCursor();
             }
         };
-        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(
-                context,
-                android.R.layout.simple_dropdown_item_1line,
-                productRepo.getProductCursor(),
-                new String[]{ProductTable.PRODUCT_NAME},
-                new int[]{android.R.id.text1},
-                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
-        );
         simpleCursorAdapter.setFilterQueryProvider(filter);
+
         simpleCursorAdapter.setCursorToStringConverter(new SimpleCursorAdapter.CursorToStringConverter() {
             @Override
             public CharSequence convertToString(Cursor cursor) {
-                Product product = new ProductCursorWrapper(cursor).getProduct();
-                return product.name;
+                return cursor.getString(cursor.getColumnIndex(ProductTable.PRODUCT_NAME));
+//                Product product = new ProductCursorWrapper(cursor).getProduct();
+//                return product.name;
             }
         });
         return simpleCursorAdapter;
